e6b01cb557e3012a6fcea7e3f92c033df1f802dd
KlimaJaroslaw <jaroslaw.klima@aprosoft.pl>
2025-03-31 23:08:48 +0200

    Interpreter

--- python/InterpreterContainers.py
+++ python/InterpreterContainers.py
@@ -1,9 +1,20 @@
-# %%
+from typing import Generic, TypeVar
 
+T = TypeVar('T')
+class InterpreterList(Generic[T]):
+    def __init__(self) -> None:
+        self.elements: list[T] = []
 
+    def add(self, element: T) -> None:
+        self.elements.append(element)
 
+    def remove(self, element: T) -> None:
+        if element in self.elements:
+            self.elements.remove()
 
-    
+    def list(self) -> list[T]:
+        return self.elements
+
 class InterpreterPoint:
     def __init__(self, x: float,y: float):
         self.x = x
@@ -16,14 +27,22 @@ class InterpreterHeight:
         self.steep = steep        
 
 class InterpreterLand:
-    def __init__(self, displacement: InterpreterPoint,  perimeter: list, height: list, perimeterFunc, heightFunc):
+    def __init__(self, displacement: InterpreterPoint,  
+                 perimeter: InterpreterList[InterpreterPoint], 
+                 height: InterpreterList[InterpreterHeight], 
+                 perimeterFunc, heightFunc):
         self.displacement = displacement
         self.perimeter = perimeter
         self.height = height
         self.perimeterFunc = perimeterFunc
         self.heightFunc = heightFunc    
+    
+    def export(self):
+        self.perimeter = self.perimeter.list()
+        self.height = self.height.list()
 
 class InterpreterWorld:
     def __init__(self, lands : InterpreterLand, size: InterpreterPoint):
         self.lands = lands
         self.size = size
+
