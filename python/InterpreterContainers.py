4ab6bf06587af17cc97075bc363b42e8dc77dfd4
KlimaJaroslaw <jaroslaw.klima@aprosoft.pl>
2025-03-31 19:36:08 +0200

    interpreter cointainers py

--- python/InterpreterContainers.py
+++ python/InterpreterContainers.py
@@ -1,12 +1,29 @@
 # %%
+
+
+
+
+    
+class InterpreterPoint:
+    def __init__(self, x: float,y: float):
+        self.x = x
+        self.y = y
+
+class InterpreterHeight:
+    def __init__(self, place: InterpreterPoint, z: float, steep: float):
+        self.place = place,
+        self.z = z,
+        self.steep = steep        
+
 class InterpreterLand:
-    def __init__(self, displacement,  perimeter, height, perimeterFunc, heightFunc):
+    def __init__(self, displacement: InterpreterPoint,  perimeter: list, height: list, perimeterFunc, heightFunc):
         self.displacement = displacement
         self.perimeter = perimeter
         self.height = height
         self.perimeterFunc = perimeterFunc
         self.heightFunc = heightFunc    
 
-
 class InterpreterWorld:
-    pass
+    def __init__(self, lands : InterpreterLand, size: InterpreterPoint):
+        self.lands = lands
+        self.size = size
